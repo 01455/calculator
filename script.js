9a2efe28e55208a128f7f6a192c912d84b37a5b7
01455 <95077343+01455@users.noreply.github.com>
2024-07-10 04:32:18 +0300

    add operate function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let number1;
+let number2;
+let operation;
+
 function add(num1, num2) {
     return num1 + num2
 }
@@ -12,4 +16,19 @@ function multiply(num1, num2) {
 
 function divide(num1, num2) {
     return num1 / num2
+}
+
+function operate(operator, num1, num2) {
+    switch(operator) {
+        case '+':
+            return add(num1,num2)
+        case '-':
+            return subtract(num1,num2)
+        case '*':
+            return multiply(num1,num2)
+        case '/':
+            return divide(num1,num2)
+        default:
+            return 'Invalid operator error'
+    }
 }
